03df1d80ec131b1b3cdfd46b87c8c02042c04f1d
chenxuan <523764720@qq.com>
2019-02-28 15:36:34 +0800

    拆分create中的函数,便于spread 指定xlsm文件时构建文件夹

--- CLIS/create.py
+++ CLIS/create.py
@@ -15,15 +15,15 @@ class base(CLIS.CLISCMDBASE.base):
     xlsm_download_url = "https://raw.githubusercontent.com/cxwithyxy/Excel_JS_Runtime/master/release/base.xlsm"
 
     def run(self):
-        self.make_path()
+        self.make_path(self.get_argu())
         self.down_xlsm()
-        self.make_ini_file()
-        self.spread_xlsm()
+        self.make_ini_file(self.get_argu())
+        self.spread_xlsm(self.get_argu())
 
-    def make_path(self):
-        mylib.path.mkdir(self.get_argu())
+    def make_path(self, proj_name):
+        mylib.path.mkdir(proj_name)
         mylib.path.mkdir(
-            mylib.path.join_path(self.get_argu(), "src")
+            mylib.path.join_path(proj_name, "src")
         )
 
     def down_xlsm(self):
@@ -33,11 +33,11 @@ class base(CLIS.CLISCMDBASE.base):
             if chunk:
                 f.write(chunk)
 
-    def make_ini_file(self):
-        PJI.projIni().set_base_path(self.get_argu() + "/")
-        PJI.projIni().init_config_file(self.get_argu())
+    def make_ini_file(self, proj_name):
+        PJI.projIni().set_base_path(proj_name + "/")
+        PJI.projIni().init_config_file(proj_name)
 
-    def spread_xlsm(self):
+    def spread_xlsm(self, proj_name):
         spread.base().spread_xlsm(
-            str(mylib.path.join_path(self.get_argu(), self.get_argu() + ".xlsm"))
+            str(mylib.path.join_path(proj_name, proj_name + ".xlsm"))
         )
--- CLIS/spread.py
+++ CLIS/spread.py
@@ -3,6 +3,7 @@
 import CLIS.CLISCMDBASE
 import mylib.projIni as PJI
 import mylib.xlsmHub as XH
+import create
 
 class base(CLIS.CLISCMDBASE.base):
 
@@ -12,6 +13,15 @@ class base(CLIS.CLISCMDBASE.base):
         argu = self.get_argu()
         if argu == "here":
             self.spread_here()
+            return
+        self.spread_exist_xlsm(argu)
+
+    def spread_exist_xlsm(self, xlsm_path):
+        
+        create.base().make_path(xlsm_path)
+        # create.base().down_xlsm(xlsm_path)
+        create.base().make_ini_file(xlsm_path)
+        # create.base().spread_xlsm(xlsm_path)
 
     def spread_here(self):
         xlsm_path = PJI.projIni().get_xlsm_full_path()
