599c739dced95c94c60dcc732c841e009aa5f4d7
chenxuan <523764720@qq.com>
2019-06-25 11:06:38 +0800

    现在已经找到监控的目录的方法了

--- CLIS/watch_pack.py
+++ CLIS/watch_pack.py
@@ -3,6 +3,9 @@
 import CLIS.CLISCMDBASE
 import mylib.xlsmHub as XH
 import mylib.projIni as PJI
+import win32.win32api as win32api
+import win32.win32event as win32event
+import mylib.path as mypath
 
 class base(CLIS.CLISCMDBASE.base):
 
@@ -12,9 +15,18 @@ class base(CLIS.CLISCMDBASE.base):
         argu = self.get_argu()
         if argu == "here":
             print("222")
+            self.file_change()
             # xlsm_path = PJI.projIni().get_xlsm_full_path()
             # XH.XlsmHub().set_xlsm_path(xlsm_path)
             # XH.XlsmHub().open()
             # XH.XlsmHub().input_js_file()
             # XH.XlsmHub().close()
             return
+    def file_change(self):
+        while(True):
+            handler = win32api.FindFirstChangeNotification(mypath.abs_path(""), True, 0x00000010)
+            print(handler)
+            kee = win32api.FindNextChangeNotification(handler)
+            print(kee)
+            win32event.WaitForSingleObject(handler, -1)
+
